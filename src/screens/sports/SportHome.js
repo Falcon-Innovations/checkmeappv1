fd04e57dde97b34a5d9f840d67c3e977f8a13988
yuyunfrancis <francisberi@yahoo.com>
2023-01-01 16:41:15 +0100

    sports-initiatied

---
+++ src/screens/sports/SportHome.js
@@ -0,0 +1,14 @@
+import {StyleSheet, Text, View} from 'react-native';
+import React from 'react';
+
+const SportHome = () => {
+  return (
+    <View>
+      <Text>SportHome</Text>
+    </View>
+  );
+};
+
+export default SportHome;
+
+const styles = StyleSheet.create({});
